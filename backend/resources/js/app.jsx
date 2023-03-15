f88c6a091ad4b237d2478af5f046cf61d685449b
y_ota <omtia.cyu@gmail.com>
2023-03-15 11:46:05 +0900

    Material-UI導入

--- backend/resources/js/app.jsx
+++ backend/resources/js/app.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
+import Button from '@mui/material/Button';
 
 const App = () => {
-    return <h1>Hell World</h1>;
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">React導入できたわな？？</div>
+
+                        <div className="card-body">I'm an example component!</div>
+                        <Button color="primary" variant="contained">Hello World</Button> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("app"));

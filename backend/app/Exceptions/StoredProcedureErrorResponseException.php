d2c359214b877f992036e4bbb651dc928fa7c0cc
y_ota <omtia.cyu@gmail.com>
2023-07-28 15:15:30 +0900

    Exception修正

--- backend/app/Exceptions/StoredProcedureErrorResponseException.php
+++ backend/app/Exceptions/StoredProcedureErrorResponseException.php
@@ -7,7 +7,7 @@ class StoredProcedureErrorResponseException extends BaseErrorResponseException
     public function toResponse($request)
     {
         $this->setErrorMessage('ストアドプロシージャ内でエラーが発生しました。\n'. $this->getErrorMessage());
-        $this->setStatusCode(500);
+        $this->setStatusCode(505);
         $this->setErrorCode('stored_procedure');
         return parent::toResponse($request);
     }

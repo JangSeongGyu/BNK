62bb27d38e78be685639ed5b71e064716e861178
JangSeongGyu <jang.seongyu@mic-p.com>
2023-08-02 18:19:04 +0900

    Add Header Drawer

--- backend/resources/js/components/Header.jsx
+++ backend/resources/js/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -8,49 +8,19 @@ import MenuIcon from '@mui/icons-material/Menu';
 import FunctionList from '../components/FunctionList';
 import { blue, grey, orange, pink } from '@mui/material/colors';
 import { Divider, Drawer } from '@mui/material';
-import LocalTaxiIcon from '@mui/icons-material/LocalTaxi';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useNavigate } from 'react-router-dom';
+import HeaderDrawer from './HeaderDrawer';
 
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 const Header = (props) => {
-    let navigate = useNavigate();
     const pageType = props.pageType;
     const page = props.page;
     const [title, SetTitle] = useState('');
     const disableList = props.disableList;
-    const [open, setOpen] = useState(false);
-
-    const menuItemOption = () => {
-        return {
-            borderBottom: 1,
-            borderColor: grey[400],
-            display: 'flex',
-            alignItems: 'center',
-            px: 2,
-            height: 60,
-            ':hover': { backgroundColor: grey[200] },
-        };
-    };
-
-    const menuItemTextOption = (menuType) => {
-        let selectMenu;
-        if (pageType == menuType) selectMenu = blue[500];
-        else selectMenu = 'black';
-
-        return {
-            color: selectMenu,
-        };
-    };
-
-    const close = () => {
-        setOpen(false);
-    };
+    const drawerRef = useRef('');
 
     useEffect(() => {
         if (pageType == 'supermarket') SetTitle('スーパーマーケット');
         else if (pageType == 'taxi') SetTitle('タクシー');
-        else if (pageType == 'eagles') SetTitle('イーグルス');
     }, []);
 
     return (
@@ -62,7 +32,9 @@ const Header = (props) => {
                         color="inherit"
                         aria-label="menu"
                         sx={{ mr: 2 }}
-                        onClick={() => setOpen(true)}
+                        onClick={() =>
+                            drawerRef.current.handleClickOpen(pageType)
+                        }
                     >
                         <MenuIcon />
                     </IconButton>
@@ -80,63 +52,7 @@ const Header = (props) => {
                 )}
             </AppBar>
 
-            <Drawer open={open} onClose={() => close()}>
-                <Box height={'100%'} width={300}>
-                    <Box
-                        onClick={() => close()}
-                        backgroundColor={pink[500]}
-                        py={2}
-                        boxShadow={2}
-                        fontWeight={'bold'}
-                        color={'white'}
-                        display={'flex'}
-                        alignItems={'center'}
-                        sx={{
-                            ':hover': { background: pink[400] },
-                        }}
-                    >
-                        <ArrowBackIosNewIcon sx={{ fontSize: 30 }} />
-                        <Typography
-                            width={'88%'}
-                            textAlign={'center'}
-                            fontSize={24}
-                        >
-                            {/* 楽天業務システム */}
-                        </Typography>
-                    </Box>
-                    <Box
-                        onClick={() => navigate('/supermarket')}
-                        sx={menuItemOption()}
-                    >
-                        <ShoppingCartIcon
-                            sx={{ color: pink[500], fontSize: 30, mr: 1 }}
-                        />
-                        <Typography
-                            sx={menuItemTextOption('supermarket')}
-                            textAlign={'center'}
-                            my={2}
-                            fontSize={24}
-                        >
-                            スーパーマーケット
-                        </Typography>
-                    </Box>
-                    <Box
-                        onClick={() => navigate('/taxi')}
-                        sx={menuItemOption()}
-                    >
-                        <LocalTaxiIcon
-                            sx={{ color: orange[600], fontSize: 30, mr: 1 }}
-                        />
-                        <Typography
-                            textAlign={menuItemTextOption('taxi')}
-                            my={2}
-                            fontSize={24}
-                        >
-                            タクシー
-                        </Typography>
-                    </Box>
-                </Box>
-            </Drawer>
+            <HeaderDrawer ref={drawerRef} />
         </Box>
     );
 };

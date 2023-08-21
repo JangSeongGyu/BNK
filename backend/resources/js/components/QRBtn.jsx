844d81deb650f8871995644f1c1da17a59604fda
JangSeongGyu <jang.seongyu@mic-p.com>
2023-08-21 09:08:43 +0900

    Update JobTicket

--- backend/resources/js/components/QRBtn.jsx
+++ backend/resources/js/components/QRBtn.jsx
@@ -27,7 +27,8 @@ const QRBtn = (props) => {
                     return b.問い合わせ番号 - a.問い合わせ番号;
                 });
                 toast.success('エクスポート完了。', { id: toastId });
-                download({ supermarket: MakeExcel(sorted) });
+
+                download({ supermarket: SPMakeExcel(sorted) });
             })
             .catch((e) => {
                 let errMsg = '';
@@ -46,13 +47,14 @@ const QRBtn = (props) => {
             .get(`/api/${pageType}/qrdata/${selectDate}`)
             .then((res) => {
                 const qrData = res.data;
+                console.log(res.data);
                 Object.keys(qrData).forEach((key) => {
                     if (qrData[key].length == 0) return;
                     let data = qrData[key];
                     const sorted = data.sort((a, b) => {
                         return b.問い合わせ番号 - a.問い合わせ番号;
                     });
-                    workbooks[key] = MakeExcel(sorted);
+                    workbooks[key] = TXMakeExcel(sorted);
                 });
                 download(workbooks);
 
@@ -69,7 +71,7 @@ const QRBtn = (props) => {
             });
     };
 
-    const MakeExcel = (qrDatas) => {
+    const TXMakeExcel = (qrDatas) => {
         console.log('MakeExcel', qrDatas);
         let workbook = new ExcelJS.Workbook();
         let worksheet = workbook.addWorksheet('DTF連携', {});
@@ -79,7 +81,70 @@ const QRBtn = (props) => {
 
         // HEADER
         let headerData = [
-            '印字コード',
+            '印字CODE',
+            '納品先宛名',
+            '印字名称',
+            '画像データ名',
+            '間紙フラグ',
+            '納品数量',
+            '間紙バーコード',
+            'つぶし',
+            '連番',
+        ];
+
+        let headerRow = worksheet.getRow(1);
+        headerData.forEach((data, index) => {
+            headerRow.getCell(index + 1).value = data;
+        });
+
+        // BODY
+        for (let i = 0; i < qrDatas.length; i++) {
+            let sheetRow = worksheet.getRow(cnt++);
+            // CONTENT　HEADER
+
+            sheetRow.getCell(2).value = qrDatas[i].納品先宛名;
+            sheetRow.getCell(3).value = qrDatas[i].ショップ名;
+            sheetRow.getCell(5).value = 1;
+            sheetRow.getCell(6).value = qrDatas[i].数量;
+            sheetRow.getCell(7).value =
+                'a' +
+                qrDatas[i].問い合わせ番号 +
+                paddingNum(qrDatas[i].同梱連番, 3) +
+                'a';
+
+            sheetRow.getCell(8).value = qrDatas[i].つぶし;
+
+            sheetRow.getCell(9).value =
+                paddingNum(qrDatas[i].数量, 4) +
+                '/' +
+                paddingNum(qrDatas[i].数量, 4);
+
+            // CONTENT　BODY
+            for (let j = 1; j <= qrDatas[i].数量; j++) {
+                let secondRow = worksheet.getRow(cnt++);
+                secondRow.getCell(1).value =
+                    qrDatas[i].ショップコード + qrDatas[i].シーンコード;
+                secondRow.getCell(3).value = qrDatas[i].シーン名;
+                secondRow.getCell(4).value = qrDatas[i].URL;
+                secondRow.getCell(8).value = qrDatas[i].つぶし;
+                secondRow.getCell(9).value =
+                    paddingNum(j, 4) + '/' + paddingNum(qrDatas[i].数量, 4);
+            }
+        }
+        return workbook;
+    };
+
+    const SPMakeExcel = (qrDatas) => {
+        console.log('MakeExcel', qrDatas);
+        let workbook = new ExcelJS.Workbook();
+        let worksheet = workbook.addWorksheet('DTF連携', {});
+        let cnt = 2;
+
+        // qrData.sort(qrData.問い合わせ番号);
+
+        // HEADER
+        let headerData = [
+            '印字CODE',
             '印字名称',
             '画像データ名',
             '間紙フラグ',
